820b543a6716240ddecc9e2c0a8cdd5e2b39c589
Justine Jose <54615524+JustineExperion@users.noreply.github.com>
2020-05-22 14:19:24 +0530

    Can I fork and use this repo
    pls reply as comment or send mail to justnshalom@gmail.com

--- CoreUI/Startup.cs
+++ CoreUI/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
+////Can I fork and use this repo
 namespace CoreUI
 {
     public class Startup
